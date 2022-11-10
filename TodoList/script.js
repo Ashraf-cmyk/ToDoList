addcd90d77023205ee08f4e0f4c490afcb3a4537
Ashraf-cmyk <106589086+Ashraf-cmyk@users.noreply.github.com>
2022-11-10 19:21:09 +0100

    TodoList Winc_Academy
    Hallo ik kon de local_api niet toevoegen die was te groot krijg een foutmelding! was te groot te veel files.!

---
+++ TodoList/script.js
@@ -0,0 +1,70 @@
+////////////////callList/////////////////////
+
+async function callTodolist() {
+  try {
+    const todoList = await getTodolist();
+    console.log(todoList);
+    const list = document.querySelector("#task-list");
+    const addTasksToDom = todoList
+      .map(function (task) {
+        newArray =
+          "<li id=" +
+          task._id +
+          '> <input type="checkbox" id="check"><span title="Double click to change task text">' +
+          task.description +
+          '</span><img class="delete" src="Yourtrash.png" /></li>';
+        return newArray;
+      })
+      .join("");
+
+    ////////////addWrittenListToDom//////////////
+
+    list.innerHTML = addTasksToDom;
+    let listItem = document.querySelectorAll("#task-list li img.delete");
+    listItem.forEach(function (task) {
+      task.addEventListener("click", function (e) {
+        const id = e.target.parentNode.getAttribute("id");
+        deleteTask(id);
+      });
+    });
+
+    let changeItem = document.querySelectorAll("#task-list li span");
+    changeItem.forEach(function (task) {
+      task.addEventListener("click", function (e) {
+        const taskText = e.target;
+        taskText.setAttribute("contenteditable", "true");
+        var contenteditable = document.querySelector("[contenteditable]"),
+          text = contenteditable.textContent;
+        const id = taskText.parentNode.getAttribute("id");
+        changeTask(id, text);
+      });
+    });
+
+    ////////////BonusCrossWordsTrash//////////////
+
+    let crossItem = document.querySelectorAll("#task-list li input#check");
+    crossItem.forEach(function (task) {
+      task.addEventListener("click", function (e) {
+        const textLI = e.target.parentNode;
+        textLI.classList.toggle("strike");
+        const id = textLI.getAttribute("id");
+        doneTask(id);
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+callTodolist();
+
+////////////postButton//////////////
+
+const btn = document.querySelector("#mybutton");
+
+btn.addEventListener("click", () => {
+  let value = document.getElementById("task").value;
+
+  addTask(value);
+  callTodolist();
+});
